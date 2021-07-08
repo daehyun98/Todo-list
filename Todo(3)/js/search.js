a7f674bb304b3e57ce8d9d57b3c3979a877ce253
daehyun98 <ksmm9100@gamil.com>
2021-07-08 16:57:27 +0900

    0708

--- Todo(3)/js/search.js
+++ Todo(3)/js/search.js
@@ -2,7 +2,7 @@ const search = document.querySelector('#js-search');
 const searchInput = search.querySelector('input');
 search.addEventListener('submit', function(e){
     e.preventDefault();
-    window.location.href = "https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query="+searchInput.value;
+    window.location.href = `https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=${searchInput.value}`;
     searchInput.value = '';
     return false;
 });
