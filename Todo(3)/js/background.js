a860921898af346ada097f86899f1f64c9ab7e33
daehyun98 <ksmm9100@gamil.com>
2021-07-12 18:41:32 +0900

    0712

--- Todo(3)/js/background.js
+++ Todo(3)/js/background.js
@@ -5,6 +5,7 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 
 bgImage.src = 'img/0.jpeg','img/1.jpeg';
+//백틱기호 못써서 바꿈
 bgImage.alt = 'bgimg';
 // `img/${chosenImage}`;
 
--- Todo(3)/js/clock.js
+++ Todo(3)/js/clock.js
@@ -26,6 +26,7 @@ function getClock() {
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
+  // 백틱 기호 못써서 바꿈
   clock.innerText = hours+":"+minutes+":"+seconds;
 }
 
