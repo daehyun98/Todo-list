d0c02d378c69a24d5ef215493673698225db0bef
daehyun98 <ksmm9100@gamil.com>
2021-07-21 17:25:39 +0900

    0721

---
+++ (김대현)ui구현1차/js/background.js
@@ -0,0 +1,12 @@
+const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = 'img/9.jpg.jpg','img/1.jpeg';
+//백틱기호 못써서 바꿈
+bgImage.alt = 'bgimg';
+// `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
---
+++ (김대현)ui구현1차/js/clock.js
@@ -0,0 +1,34 @@
+//IE에서 padStart()를 실행시키기 위해서는 MDN에서 제공하는 폴리필을 다른 모든 코드 이전에 추가시켜야 한다.
+
+// https://github.com/uxitten/polyfill/blob/master/string.polyfill.js
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+if (!String.prototype.padStart) {
+    String.prototype.padStart = function padStart(targetLength,padString) {
+        targetLength = targetLength>>0; //truncate if number or convert non-number to 0;
+        padString = String((typeof padString !== 'undefined' ? padString : ' '));
+        if (this.length > targetLength) {
+            return String(this);
+        }
+        else {
+            targetLength = targetLength-this.length;
+            if (targetLength > padString.length) {
+                padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
+            }
+            return padString.slice(0,targetLength) + String(this);
+        }
+    };
+}
+
+const clock = document.querySelector("h2#clock");
+
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  // 백틱 기호 못써서 바꿈
+  clock.innerText = hours+":"+minutes+":"+seconds;
+}
+
+getClock();
+setInterval(getClock, 1000);
