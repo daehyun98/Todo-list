b31e94706aaedd11378a87c3416990f74dc79b3c
daehyun98 <ksmm9100@gamil.com>
2021-09-02 18:01:10 +0900

    0902

--- (김대현)ui구현1차/js/todo.js
+++ (김대현)ui구현1차/js/todo.js
@@ -13,6 +13,7 @@ function saveToDos() {
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
+  //filter IE지원x 바꿔야함
   toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
   saveToDos();
 }
