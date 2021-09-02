7731f9797d9ddbcacd4572645c2e5910139cfb32
daehyun98 <ksmm9100@gamil.com>
2021-09-02 17:53:35 +0900

    0902

--- (김대현)ui구현1차/js/search.js
+++ (김대현)ui구현1차/js/search.js
@@ -2,7 +2,9 @@ const search = document.querySelector('#js-search');
 const searchInput = search.querySelector('input');
 search.addEventListener('submit', function(e){
     e.preventDefault();
-    window.location.href = `https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=${searchInput.value}`;
+    //IE 호환 문제 백틱기호 안쓰고 바꿈
+    window.location.href = "https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query="+searchInput.value;
+    // window.location.href = `https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=${searchInput.value}`;
     searchInput.value = '';
     return false;
 });
